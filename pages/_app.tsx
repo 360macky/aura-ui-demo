234ec7988c2f1a6e6d31ee3dc701c8923296379a
Marcelo Arias <hello@marceloarias.com>
2022-12-07 21:55:15 -0500

    Add Navbar component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
+import '../styles/styles.css'
+
+import { Inter } from '@next/font/google'
 import type { AppProps } from 'next/app'
+import { AuraUIProvider } from '@guruhotel/aura-ui'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <main className={inter.className}><AuraUIProvider><Component {...pageProps} /></AuraUIProvider></main>
 }
